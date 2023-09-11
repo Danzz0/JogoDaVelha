d53f382df9f3b1cb5e30b03396303245d354877c
Danilo Lima~ <danilo.limagomes10@gmail.com>
2023-09-10 22:46:00 -0300

    Acrescentando o método main

--- app/ts/JogoDaVelha.ts
+++ app/ts/JogoDaVelha.ts
@@ -8,17 +8,37 @@ const leia = readline.createInterface({
 
 class JogoDaVelha{
 
-
-    public tabu:Campo = new Campo[3][3]();
+    public tabu:Campo[][] = new Array<Array<Campo>>(3).fill([]).map(() => new Array<Campo>(3));
     public game:boolean = true;
     public jogadorAtual:string = 'X';
+    public vitoria:string =""; 
+    
 
 
-    getGame():boolean{
+    public main():void{
+
+        while(this.game){
+            this.geraTabu(this.tabu);
+            this.vitoria = this.verificarVitoria(this.tabu);
+            if(this.vitoria != ""){
+                console.log(`Jogador ${this.vitoria} venceu`);
+            } 
+            try{
+        
+            } catch(err){
+                console.log("ERRO")
+            }
+        }
+    }
+
+    public getGame():boolean{
         return this.game;
     }
 
-    public static geraTabu(tabu:Campo[][]):void{
+    
+
+    public geraTabu(tabu:Campo[][]):void{
+        this.limparTela();
         console.log(`   0  1  2 `);
         console.log(` 0  ${tabu[0][0].getJogador()} | ${tabu[0][1].getJogador()} | ${tabu[0][2].getJogador()} `);
         console.log("  ------------------");
@@ -27,10 +47,24 @@ class JogoDaVelha{
         console.log(` 0  ${tabu[2][0].getJogador()} | ${tabu[2][1].getJogador()} | ${tabu[2][2].getJogador()} `);
 
     }
-}
 
-do{
-    var jogo = new JogoDaVelha();
-} while(jogo.getGame()){
+    public limparTela():void{
+        for(let i = 0; i<200; i++){
+            console.log("");
+        }
+    }
 
+
+
+    public static verificarJogada(){
+
+    }
+
+
+    // Função para verificar a vitória de algum jogador (Ainda irá ser implementado)
+    public verificarVitoria(tabu:Campo[][]):string{
+        return "";
+    }
+
+   
 }
