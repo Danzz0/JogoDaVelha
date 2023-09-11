3b623abb0d475eebf61155431b49dc58f7b4f481
Danilo Lima~ <danilo.limagomes10@gmail.com>
2023-09-10 22:49:06 -0300

    Transpilando para Js

--- app/js/JogoDaVelha.js
+++ app/js/JogoDaVelha.js
@@ -1,6 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var Campo_1 = require("./Campo");
 var readline = require('readline');
 var leia = readline.createInterface({
     input: process.stdin,
@@ -8,14 +7,30 @@ var leia = readline.createInterface({
 });
 var JogoDaVelha = /** @class */ (function () {
     function JogoDaVelha() {
-        this.tabu = new Campo_1.Campo[3][3]();
+        this.tabu = new Array(3).fill([]).map(function () { return new Array(3); });
         this.game = true;
         this.jogadorAtual = 'X';
+        this.vitoria = "";
     }
+    JogoDaVelha.prototype.main = function () {
+        while (this.game) {
+            this.geraTabu(this.tabu);
+            this.vitoria = this.verificarVitoria(this.tabu);
+            if (this.vitoria != "") {
+                console.log("Jogador ".concat(this.vitoria, " venceu"));
+            }
+            try {
+            }
+            catch (err) {
+                console.log("ERRO");
+            }
+        }
+    };
     JogoDaVelha.prototype.getGame = function () {
         return this.game;
     };
-    JogoDaVelha.geraTabu = function (tabu) {
+    JogoDaVelha.prototype.geraTabu = function (tabu) {
+        this.limparTela();
         console.log("   0  1  2 ");
         console.log(" 0  ".concat(tabu[0][0].getJogador(), " | ").concat(tabu[0][1].getJogador(), " | ").concat(tabu[0][2].getJogador(), " "));
         console.log("  ------------------");
@@ -23,10 +38,16 @@ var JogoDaVelha = /** @class */ (function () {
         console.log("  ------------------");
         console.log(" 0  ".concat(tabu[2][0].getJogador(), " | ").concat(tabu[2][1].getJogador(), " | ").concat(tabu[2][2].getJogador(), " "));
     };
+    JogoDaVelha.prototype.limparTela = function () {
+        for (var i = 0; i < 200; i++) {
+            console.log("");
+        }
+    };
+    JogoDaVelha.verificarJogada = function () {
+    };
+    // Função para verificar a vitória de algum jogador (Ainda irá ser implementado)
+    JogoDaVelha.prototype.verificarVitoria = function (tabu) {
+        return "";
+    };
     return JogoDaVelha;
 }());
-do {
-    var jogo = new JogoDaVelha();
-} while (jogo.getGame());
-{
-}
